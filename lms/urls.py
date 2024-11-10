22447ac9a85aa57d63029b01db05a22f78d5bcb1
admin <codenamekirkpatrick@yandex.ru>
2024-11-10 22:55:37 +0300

    done homework 24.2*

--- lms/urls.py
+++ lms/urls.py
@@ -14,7 +14,7 @@ from lms.views import (
 app_name = LmsConfig.name
 
 router = SimpleRouter()
-router.register("", CourseViewSet)
+router.register("course", CourseViewSet)
 
 urlpatterns = [
     path("lessons/", LessonListAPIView.as_view(), name="lesson_list"),
